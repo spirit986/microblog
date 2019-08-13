c1dd26205544b8ef5da4847069b62a4948c323f3
Tome Petkovski <spirit.986@gmail.com>
2019-08-13 13:21:59 +0000

    Define database schema

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app_microblog = Flask(__name__)
 app_microblog.config.from_object(Config)
+db = SQLAlchemy(app_microblog)
+migrate = Migrate(app_microblog, db)
 
-from app import routes
+from app import routes, models
+
